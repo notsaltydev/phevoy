0e0a81b4ae4d9687e8eb0ac6df38041da91597b8
notsaltydev <notsaltydev@gmail.com>
2020-05-09 14:28:33 +0200

    [Meet] Closing conference after hangup.

--- src/app/meet/src/components/meet/meet.component.ts
+++ src/app/meet/src/components/meet/meet.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
+import { fromEventPattern, Observable, Subscription } from 'rxjs';
+import { EventEmitter } from 'events';
 
 declare const JitsiMeetExternalAPI;
 
@@ -9,8 +11,10 @@ declare const JitsiMeetExternalAPI;
     styleUrls: ['./meet.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MeetComponent implements OnInit {
+export class MeetComponent implements OnInit, OnDestroy {
     @ViewChild('meet', {static: true}) private meetRef: ElementRef;
+    private jitsiMeetExternalAPI: any;
+    private subscription: Subscription = new Subscription();
 
     constructor(private route: ActivatedRoute) {
     }
@@ -26,8 +30,45 @@ export class MeetComponent implements OnInit {
                 parentNode: this.meetRef.nativeElement
             };
 
-            const api = new JitsiMeetExternalAPI(domain, options);
+            this.jitsiMeetExternalAPI = new JitsiMeetExternalAPI(domain, options);
+
+            if (this.jitsiMeetExternalAPI) {
+                this.bindEvents();
+            }
         });
+
+    }
+
+    @HostListener('window:beforeunload', ['$event'])
+    onBeforeUnload() {
+        this.disposeConference();
+    }
+
+    private bindEvents() {
+        this.subscription.add(this.handleJitsiEvent('readyToClose').subscribe(
+            () => {
+                this.jitsiMeetExternalAPI.dispose();
+            }
+        ));
+    }
+
+    private handleJitsiEvent<T>(eventName: string): Observable<any> {
+        return fromEventPattern<T>((handler) => this.jitsiEventHandler(eventName, handler));
+    }
+
+    private jitsiEventHandler(eventName, handler): EventEmitter {
+        return this.jitsiMeetExternalAPI.on(eventName, handler);
+    }
+
+    private disposeConference(): void {
+        if (!!this.jitsiMeetExternalAPI) {
+            this.jitsiMeetExternalAPI.dispose();
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+        this.jitsiMeetExternalAPI.dispose();
     }
 
 }
