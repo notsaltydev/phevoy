ca665f2b3df9906c5b0bbbe9c3892ac672ce6b30
notsaltydev <notsaltydev@gmail.com>
2020-05-23 23:13:23 +0200

    Home - faq.

---
+++ src/app/home/faq-items-data.ts
@@ -0,0 +1,48 @@
+export interface FaqItem {
+    header: string;
+    body: string;
+    isCollapsed: boolean;
+}
+
+export const FAQItems: FaqItem[] = [
+    {
+        header: 'How can I properly setup an account?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Can I change my plan after sign up?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Can I ask for specific features?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Can I invite people who don\'t own an account yet to my conference?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Are allowed to sign up for more than one account with the same email address?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'How much records are allowed to process monthly when on the free plan?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Do you make custom implementations?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    },
+    {
+        header: 'Can we manage user roles?',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Invidiosum nomen est, infame, suspectum. Utilitatis causa amicitia est quaesita. Non laboro, inquit, de nomine. Duo Reges: constructio interrete. Aliter homines, aliter philosophos loqui putas oportere?',
+        isCollapsed: false
+    }
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FaqItem, FAQItems } from './faq-items-data';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  form: FormGroup = new FormGroup({});
+    form: FormGroup = new FormGroup({});
+    faqItems: FaqItem[] = FAQItems;
 }
