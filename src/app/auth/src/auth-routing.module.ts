c26fe99aca1e68c099a910140ed531009dc9537b
notsaltydev <notsaltydev@gmail.com>
2020-05-24 19:03:39 +0200

    [Auth] Added base components. Setup auth routes.

--- src/app/auth/src/auth-routing.module.ts
+++ src/app/auth/src/auth-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent, LoginComponent, LogoutComponent, RegisterComponent, ResetPasswordComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent,
+        children: [
+            {
+                path: '',
+                component: LoginComponent,
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+            },
+            {
+                path: 'logout',
+                component: LogoutComponent,
+            },
+            {
+                path: 'reset-password',
+                component: ResetPasswordComponent,
+            },
+        ],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- src/app/auth/src/auth.module.ts
+++ src/app/auth/src/auth.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { AuthComponent, LoginComponent, LogoutComponent, RegisterComponent, ResetPasswordComponent } from './components';
 
 @NgModule({
     imports: [
         CommonModule,
         AuthRoutingModule
     ],
-    declarations: []
+    declarations: [
+        AuthComponent,
+        LoginComponent,
+        LogoutComponent,
+        RegisterComponent,
+        ResetPasswordComponent
+    ]
 })
 export class AuthModule {
 }
