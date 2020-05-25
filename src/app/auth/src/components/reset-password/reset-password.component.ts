ddc0def7101d119d8972b5c205bb5533eb77464b
notsaltydev <notsaltydev@gmail.com>
2020-05-25 22:33:09 +0200

    [Auth] Reset password page.

--- src/app/auth/src/components/reset-password/reset-password.component.ts
+++ src/app/auth/src/components/reset-password/reset-password.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { AuthResult, AuthService } from '../../services';
+import { AUTH_OPTIONS } from '../../auth.options';
+import { Router } from '@angular/router';
+import { getDeepFromObject } from '../../helpers';
 
 @Component({
     selector: 'app-reset-password',
     templateUrl: './reset-password.component.html',
     styleUrls: ['./reset-password.component.scss']
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent {
+    redirectDelay: number = 0;
+    showMessages: any = {};
+    strategy: string = '';
 
-    constructor() {
+    submitted = false;
+    errors: string[] = [];
+    messages: string[] = [];
+    user: any = {};
+
+    constructor(protected service: AuthService,
+                @Inject(AUTH_OPTIONS) protected options = {},
+                protected cd: ChangeDetectorRef,
+                protected router: Router) {
+
+        this.redirectDelay = this.getConfigValue('forms.requestPassword.redirectDelay');
+        this.showMessages = this.getConfigValue('forms.requestPassword.showMessages');
+        this.strategy = this.getConfigValue('forms.requestPassword.strategy');
     }
 
-    ngOnInit(): void {
+    requestPass(): void {
+        this.errors = this.messages = [];
+        this.submitted = true;
+
+        this.service.requestPassword(this.strategy, this.user).subscribe((result: AuthResult) => {
+            this.submitted = false;
+            if (result.isSuccess()) {
+                this.messages = result.getMessages();
+            } else {
+                this.errors = result.getErrors();
+            }
+
+            const redirect = result.getRedirect();
+            if (redirect) {
+                setTimeout(() => {
+                    return this.router.navigateByUrl(redirect);
+                }, this.redirectDelay);
+            }
+            this.cd.detectChanges();
+        });
     }
 
+    getConfigValue(key: string): any {
+        return getDeepFromObject(this.options, key, null);
+    }
 }
