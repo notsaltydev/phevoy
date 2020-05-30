fad9b9209bbfb7fd255d8e9a102faa98cf508de0
notsaltydev <notsaltydev@gmail.com>
2020-05-31 01:24:41 +0200

    [Auth] Auth integration - ability to login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ErrorInterceptor, JwtInterceptor } from './_helpers/interceptors';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { PageNotFoundComponent } from './page-not-found';
@@ -17,10 +16,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbDatepickerModule, NbDialogModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { AUTH_USER_OPTIONS } from './auth/src/auth.options';
 import { FeedbackComponent } from './feedback';
 import { CoreModule } from './core';
 import { PasswordAuthStrategy } from './auth/src/strategies/password';
+import { AuthJWTToken } from './auth/src/services/token';
+import { AuthModule } from './auth';
 
 const socialLinks = [
     {
@@ -54,7 +54,105 @@ const socialLinks = [
         NbDialogModule.forRoot(),
         NbDatepickerModule.forRoot(),
         CalendarModule.forRoot({provide: DateAdapter, useFactory: adapterFactory}),
-        NbMenuModule.forRoot()
+        NbMenuModule.forRoot(),
+        AuthModule.forRoot({
+            strategies: [
+                PasswordAuthStrategy.setup({
+                    name: 'email',
+                    baseEndpoint: 'http://localhost:3000',
+                    token: {
+                        class: AuthJWTToken,
+
+                        key: 'accessToken', // this parameter tells where to look for the token
+                    },
+                    login: {
+                        endpoint: '/auth/login',
+                        method: 'post',
+                        redirect: {
+
+                            success: '/app/dashboard',
+                            failure: null, // stay on the same page
+                        },
+                    },
+                    register: {
+                        endpoint: '/auth/register',
+                        method: 'post'
+                    },
+                    logout: {
+                        endpoint: '/auth/sign-out',
+                    },
+                    requestPass: {
+                        endpoint: '/auth/request-password',
+                        method: 'post'
+                    },
+                    resetPass: {
+                        endpoint: '/auth/reset-password',
+                        method: 'post'
+                    }
+                })
+            ],
+            forms: {
+                forms: {
+                    login: {
+                        redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
+                        strategy: 'email',  // strategy id key.
+                        rememberMe: true,   // whether to show or not the `rememberMe` checkbox
+                        showMessages: {     // show/not show success/error messages
+                            success: true,
+                            error: true,
+                        },
+                        socialLinks: socialLinks, // social links at the bottom of a page
+                    },
+                    register: {
+                        redirectDelay: 500,
+                        strategy: 'email',
+                        showMessages: {
+                            success: true,
+                            error: true,
+                        },
+                        terms: true,
+                        socialLinks: socialLinks,
+                    },
+                    requestPassword: {
+                        redirectDelay: 500,
+                        strategy: 'email',
+                        showMessages: {
+                            success: true,
+                            error: true,
+                        },
+                        socialLinks: socialLinks,
+                    },
+                    resetPassword: {
+                        redirectDelay: 500,
+                        strategy: 'email',
+                        showMessages: {
+                            success: true,
+                            error: true,
+                        },
+                        socialLinks: socialLinks,
+                    },
+                    logout: {
+                        redirectDelay: 500,
+                        strategy: 'email',
+                    },
+                    validation: {
+                        password: {
+                            required: true,
+                            minLength: 4,
+                            maxLength: 50,
+                        },
+                        email: {
+                            required: true,
+                        },
+                        fullName: {
+                            required: false,
+                            minLength: 4,
+                            maxLength: 50,
+                        }
+                    }
+                }
+            }
+        })
     ],
     declarations: [
         AppComponent,
@@ -65,105 +163,7 @@ const socialLinks = [
         PricingComponent,
         FeedbackComponent
     ],
-    providers: [
-        {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
-        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-        {
-            provide: AUTH_USER_OPTIONS,
-            useValue: {
-                strategies: [
-                    PasswordAuthStrategy.setup({
-                        name: 'email',
-                        baseEndpoint: 'http://localhost:3000',
-                        login: {
-                            endpoint: '/auth/login',
-                            method: 'post',
-                            redirect: {
-                                success: '/app/dashboard/',
-                                failure: null, // stay on the same page
-                            },
-                        },
-                        register: {
-                            endpoint: '/auth/register',
-                            method: 'post'
-                        },
-                        logout: {
-                            endpoint: '/auth/sign-out',
-                        },
-                        requestPass: {
-                            endpoint: '/auth/request-password',
-                            method: 'post'
-                        },
-                        resetPass: {
-                            endpoint: '/auth/reset-password',
-                            method: 'post'
-                        }
-                    })
-                ],
-                forms: {
-                    forms: {
-                        login: {
-                            redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
-                            strategy: 'email',  // strategy id key.
-                            rememberMe: true,   // whether to show or not the `rememberMe` checkbox
-                            showMessages: {     // show/not show success/error messages
-                                success: true,
-                                error: true,
-                            },
-                            socialLinks: socialLinks, // social links at the bottom of a page
-                        },
-                        register: {
-                            redirectDelay: 500,
-                            strategy: 'email',
-                            showMessages: {
-                                success: true,
-                                error: true,
-                            },
-                            terms: true,
-                            socialLinks: socialLinks,
-                        },
-                        requestPassword: {
-                            redirectDelay: 500,
-                            strategy: 'email',
-                            showMessages: {
-                                success: true,
-                                error: true,
-                            },
-                            socialLinks: socialLinks,
-                        },
-                        resetPassword: {
-                            redirectDelay: 500,
-                            strategy: 'email',
-                            showMessages: {
-                                success: true,
-                                error: true,
-                            },
-                            socialLinks: socialLinks,
-                        },
-                        logout: {
-                            redirectDelay: 500,
-                            strategy: 'email',
-                        },
-                        validation: {
-                            password: {
-                                required: true,
-                                minLength: 4,
-                                maxLength: 50,
-                            },
-                            email: {
-                                required: true,
-                            },
-                            fullName: {
-                                required: false,
-                                minLength: 4,
-                                maxLength: 50,
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    ],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
