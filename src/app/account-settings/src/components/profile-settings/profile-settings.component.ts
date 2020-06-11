230c8a50435859a3178271fed4667e692bdc6890
notsaltydev <notsaltydev@gmail.com>
2020-06-12 00:29:56 +0200

    [Quill.js] Quill.js integration - debug handling events.

--- src/app/account-settings/src/components/profile-settings/profile-settings.component.ts
+++ src/app/account-settings/src/components/profile-settings/profile-settings.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
     selector: 'app-profile-settings',
@@ -7,9 +7,34 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileSettingsComponent {
+    isQuillEditorEnabled = false;
+
+    constructor(private changeDetector: ChangeDetectorRef) {
+    }
 
     getAvatarUr(): string {
         return 'url("https://via.placeholder.com/150")';
     }
 
+    onEditorCreated($event: any): void {
+        this.isQuillEditorEnabled = true;
+        this.changeDetector.markForCheck();
+        console.log('onEditorCreated', $event);
+    }
+
+    onSelectionChanged($event: any): void {
+        console.log('onSelectionChanged', $event);
+    }
+
+    onEditorChanged($event: any) {
+        console.log('onEditorChanged', $event);
+    }
+
+    onFocus($event: any): void {
+        console.log('onFocus', $event);
+    }
+
+    onBlur($event: any): void {
+        console.log('onBlur', $event);
+    }
 }
