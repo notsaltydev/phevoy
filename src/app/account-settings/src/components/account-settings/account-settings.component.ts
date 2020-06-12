d7eaff78507d8ec66ce7d116747b5c8d2c8d1009
notsaltydev <notsaltydev@gmail.com>
2020-06-12 23:35:43 +0200

    [Account Settings] Breadcrumb - displaying current route.

--- src/app/account-settings/src/components/account-settings/account-settings.component.ts
+++ src/app/account-settings/src/components/account-settings/account-settings.component.ts
@@ -1,24 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+const accountSettingsRouteMap: Map<string, string> = new Map([
+    ['profile', 'General'],
+    ['authentication', 'Security'],
+    ['internationalization', 'Internationalization'],
+    ['notifications', 'Notifications'],
+    ['plans-and-payment', 'Plans & Payment'],
+]);
 
 @Component({
     selector: 'app-account-settings',
     templateUrl: './account-settings.component.html',
     styleUrls: ['./account-settings.component.scss']
 })
-export class AccountSettingsComponent implements OnInit {
+export class AccountSettingsComponent implements OnInit, OnDestroy {
     faBars: IconDefinition = faBars;
     isExpanded: boolean;
+    currentRoute = '...';
+    private destroy$: Subject<void> = new Subject<void>();
 
-    constructor() {
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {
     }
 
     ngOnInit(): void {
+        this.setCurrentRoute();
+        this.router.events
+            .pipe(
+                takeUntil(this.destroy$),
+                filter(event => event instanceof NavigationEnd)
+            )
+            .subscribe((routerEvent: RouterEvent) => {
+                this.setCurrentRoute();
+            });
+    }
+
+    setCurrentRoute(): void {
+        this.currentRoute = accountSettingsRouteMap.get(this.route.snapshot.children[0].url.toString());
     }
 
     toggle(): void {
         this.isExpanded = !this.isExpanded;
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 
 }
