d4cd893f2cac428ab6fffa6a44b6d896233ac80e
notsaltydev <notsaltydev@gmail.com>
2020-07-05 23:10:56 +0200

    [NgRx] Conference reducers.

---
+++ src/app/dashboard/src/store/reducers/conference.reducer.ts
@@ -0,0 +1,39 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+import { ConferenceDto } from '../../models/conference.dto';
+import { conferenceActionTypes } from '../actions/conference.action';
+
+export interface ConferenceState extends EntityState<ConferenceDto> {
+    conferencesLoaded: boolean;
+}
+
+export const adapter: EntityAdapter<ConferenceDto> = createEntityAdapter<ConferenceDto>();
+
+export const initialState = adapter.getInitialState({
+    conferencesLoaded: false
+});
+
+export const conferenceReducer = createReducer(
+    initialState,
+
+    on(conferenceActionTypes.conferencesLoaded, (state, action) => {
+        return adapter.addAll(
+            action.conferences,
+            {...state, conferencesLoaded: true}
+        );
+    }),
+
+    on(conferenceActionTypes.createConference, (state, action) => {
+        return adapter.addOne(action.conference, state);
+    }),
+
+    on(conferenceActionTypes.deleteConference, (state, action) => {
+        return adapter.removeOne(action.conferenceId, state);
+    }),
+
+    on(conferenceActionTypes.updateConference, (state, action) => {
+        return adapter.updateOne(action.update, state);
+    })
+);
+
+export const {selectAll, selectIds} = adapter.getSelectors();
